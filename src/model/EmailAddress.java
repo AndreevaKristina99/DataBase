42427f387c98c205fe1b478061194d4b179b821b
AndreevaKristina99 <62517579+AndreevaKristina99@users.noreply.github.com>
2020-05-02 21:54:44 +0300

    email'

--- src/model/EmailAddress.java
+++ src/model/EmailAddress.java
@@ -9,23 +9,25 @@ public class EmailAddress {
 
 
     public void  emailaddress(){
-    String to = "andreeva.cr1stin@yandex.ru";//от
-    String from = "kandreeva952@gmail.com";//кому-далее считать с поля формы
+    String to = "kandreeva952@gmail.com";//от
+        String password="Andr!1999";
+    String from = "andreeva.cr1stin@yandex.ru";//кому-далее считать с поля формы
     String host = "587";//хост яндекса
     Properties properties = System.getProperties();
         properties.setProperty("smtp.yandex.ru", host);//протокол смтп яндекс
 
-     Session session = Session.getDefaultInstance(properties);
+     Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+         protected PasswordAuthentication getPasswordAuthentication() {
+             return new PasswordAuthentication("andreeva.cr1stin@yandex.ru", "Andr!1999"); //логин и пароль для аутентификации на сервере
+         }
+
+     });
         try {
         MimeMessage message = new MimeMessage(session);
-
-        message.setFrom(new InternetAddress(from));
-
+        message.setFrom(new InternetAddress(from));//от
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
         message.setSubject("Тема");//тема
         message.setText("Текст сообщения");//сообщение-даллее добавить файл сформированный
-
         Transport.send(message);
         System.out.println("Email Sent successfully....");
     }
